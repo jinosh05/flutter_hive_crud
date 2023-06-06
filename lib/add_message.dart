bfc2ff5ce4bfb004845f3ad7e2c7107f3bd89d1f
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-06-06 10:02:04 +0530

    Form fields added

--- lib/add_message.dart
+++ lib/add_message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AddMessageScreen extends StatefulWidget {
   const AddMessageScreen({super.key});
@@ -8,14 +9,36 @@ class AddMessageScreen extends StatefulWidget {
 }
 
 class _AddMessageScreenState extends State<AddMessageScreen> {
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _age = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add Message UI"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _name,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              controller: _age,
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar:
           ElevatedButton(onPressed: () {}, child: const Text("Add Message")),
     );
   }
 }
+
+
+// 
